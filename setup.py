7dcc700f286160f4075a2840e6093744a2dcec59
Toby Fey <tobyfey@protonmail.com>
2024-12-19 11:12:31 -0500

    removed discovery from interview order, attachment enabled and sections

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.MOHUDEvictionProject',
       url='https://motenanthelp.org/',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=3.1.0'],
+      install_requires=['docassemble.AssemblyLine>=3.2.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MOHUDEvictionProject/', package='docassemble.MOHUDEvictionProject'),
      )
